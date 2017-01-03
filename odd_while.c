811111df2a94d892211036bd7f88b0314bfcc5fc
GARCIA, JAMES KEVIN J <vinlog_21@yahoo.com>
2017-01-03 08:59:43 +0800

    Create odd_while.c

---
+++ odd_while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+   
+int main() {  
+    int counter; 
+    counter = 1;
+    while (counter <= 100) {  
+        printf("%d ", counter);
+     
+        counter = counter + 2;  
+    }  
+   
+    return 0;  
+} 
