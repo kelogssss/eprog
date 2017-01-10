27887406fcb80a51b2f30d110ff4c88b3b9c1462
GARCIA, JAMES KEVIN J <vinlog_21@yahoo.com>
2017-01-10 08:29:06 +0800

    Create MMR.c

---
+++ MMR.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+	int a, b , c ;
+	
+		printf("input mmr: ");
+		scanf("%d",&a);
+
+		printf("input mmr: ");
+		scanf("%d",&b);
+
+		c = abs(a-b);
+		printf("MMR Difference: %d");
+}
